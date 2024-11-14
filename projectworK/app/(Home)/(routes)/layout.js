568fa1daf42e1b84126ac940e567488b821bfa85
Harshit_gaur <harshitgaur737@gmail.com>
2024-11-15 01:11:15 +0530

    sidebar completed

--- projectworK/app/(Home)/(routes)/layout.js
+++ projectworK/app/(Home)/(routes)/layout.js
@@ -3,7 +3,7 @@ import SideBarNav from '../_components/SideBarNav'
 
 function Homelayout({children}) {
   return (
-    <div className='h-full w-64 flex-col fixed *:inset-y-0 z-50'>
+    <div className='h-full w-64 md:flex flex-col fixed *:inset-y-0 z-50 hidden'>
       <SideBarNav/>
       {children}</div>
   )
--- projectworK/app/(Home)/_components/SideBarNav.jsx
+++ projectworK/app/(Home)/_components/SideBarNav.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image';
 import { icons, Layout, Mail, Search, Shield } from 'lucide-react';
 function SideBarNav() {
@@ -28,18 +29,26 @@ function SideBarNav() {
             path: '/Newsletter',
         },
     ];
+const [activeIndex,setactiveIndex]=useState(0);
     
   return (
     <div className='h-full bg-white border-r flex-col overflow-y-auto shadow-md'>
     <div className='p-5 border-b'>
        <Image src="/logo.webp" alt="Logo" width={100} height={100} />
     </div>
-    <div className='flex flex-col'>
-        {menulist.map((item,index)=>{
-            <button>
+    
+    <div className=' flex flex-col'>
+        {menulist.map((item,index)=>(
+            <div className={`flex gap-2 items-center p-4 px-6 text-gray-500 hover:bg-gray-100 cursor-pointer${activeIndex==index ?' bg-purple-50 text-purple-500':null}`}
+            onClick={()=>setactiveIndex(index)}
+            >
+            
+                <item.icon/>
                 <h2>{item.name}</h2>
-            </button>
-        })}
+                
+            </div>
+
+        ))}
     </div>
     </div>
   )
