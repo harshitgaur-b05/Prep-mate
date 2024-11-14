3086c1f085c833a17cdf42f0235b200ae11a9073
Harshit_gaur <harshitgaur737@gmail.com>
2024-11-15 00:46:10 +0530

    add files

--- projectworK/app/(Home)/_components/SideBarNav.jsx
+++ projectworK/app/(Home)/_components/SideBarNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import { icons, Search } from 'lucide-react';
+import { icons, Layout, Mail, Search, Shield } from 'lucide-react';
 function SideBarNav() {
     const menulist = [
         {
@@ -12,19 +12,19 @@ function SideBarNav() {
         {
             id: 2,
             name: 'Dashboard',
-            icon: Search,
+            icon: Layout,
             path: '/dashboard',
         },
         {
             id: 3,
             name: 'Upgrade',
-            icon: Search,
+            icon: Shield,
             path: '/upgrade',
         },
         {
             id: 4,
             name: 'Newsletter',
-            icon: Search,
+            icon: Mail,
             path: '/Newsletter',
         },
     ];
@@ -33,6 +33,13 @@ function SideBarNav() {
     <div className='h-full bg-white border-r flex-col overflow-y-auto shadow-md'>
     <div className='p-5 border-b'>
        <Image src="/logo.webp" alt="Logo" width={100} height={100} />
+    </div>
+    <div className='flex flex-col'>
+        {menulist.map((item,index)=>{
+            <button>
+                <h2>{item.name}</h2>
+            </button>
+        })}
     </div>
     </div>
   )
