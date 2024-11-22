544cf9fdf9106006ca42e91bfc1544636e7b93e1
Harshit_gaur <harshitgaur737@gmail.com>
2024-11-23 03:29:28 +0530

    integrated graph ql

--- projectworK/app/_services/index.jsx
+++ projectworK/app/_services/index.jsx
@@ -1,20 +1,31 @@
-import {gql} from "graphql-request"
-const MASTER_URL="https://ap-south-1.cdn.hygraph.com/content/"+NEXT_PUBLIC_HYGRAPH_KEY+"/master"
+import { gql } from 'graphql-tag';
+import { request } from 'graphql-request';
 
-export  const getCourseList=async ()=>{
-    const query=gql`
+const MASTER_URL = `https://ap-south-1.cdn.hygraph.com/content/${process.env.NEXT_PUBLIC_HYGRAPH_KEY}/master`;
+
+export const getCourseList = async () => {
+  const query = gql`
     query courseList {
-  courseLists {
-    banner {
-      url
+      courseLists {
+        banner {
+          url
+        }
+        free
+        id
+        name
+        totalChapter
+        tags
+      }
     }
-    free
-    id
-    name
-    totalChapter
-    tags
+  `;
+
+  try {
+    const result = await request(MASTER_URL, query);
+    return result;
+  } catch (error) {
+    console.error('Error fetching course list:', error);
+    throw error;
   }
-}
-  `
-  const result=await request(MASTER_URL,query)
-}
+};
+
+
